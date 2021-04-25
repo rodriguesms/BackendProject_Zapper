f548579a9af7ef5ae96d36242de3b6e12fe29018
rodriguesms <miguelrodrigues@eng.ci.ufpb.br>
2021-04-25 17:17:25 -0300

    Fixing database url error

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from privateDatabaseAccess import SQLALCHEMY_DATABASE_URL
+from .privateDatabaseAccess import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
